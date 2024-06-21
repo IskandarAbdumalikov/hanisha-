1ebfb2bc147caf28ab90414087efea539f7c2c90
abdullokhpro <abdulloxrasulov441@gmail.com>
2024-06-21 15:24:27 +0500

    asasd

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import bgImg from "../../assets/images/login-bg.png";
+
+import "./login.scss";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login">
+      <div className="container login__container">
+        <form className="login__form" action="">
+          <h2>Log In</h2>
 
-export default Login
+          <label className="login__form__label">
+            Name
+            <input className="login__form__name" type="text" />
+          </label>
+          <label className="login__form__label">
+            E-mail
+            <input className="login__form__password" type="password" />
+          </label>
+          <button className="login__btn">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
