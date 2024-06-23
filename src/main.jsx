74e58c9f97eb25ac26d20ac5e6bedf351a7605be
iskandarprogrammer2007 <abdumalikoviskandar600@gmail.com>
2024-06-23 19:40:01 +0500

    added

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./context/store.js";
-import 'number-brm'
+// import numberBrm from "number-brm";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
